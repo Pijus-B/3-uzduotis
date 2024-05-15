81c9fa6f2a2588b3784ca8c0ed970762f5ad98fd
Pijus-B <p.bogusi@gmail.com>
2024-05-15 11:44:50 +0300

    copy+move operatoriai

--- Vector/vector.h
+++ Vector/vector.h
@@ -23,11 +23,25 @@ class Vector{
     explicit Vector (size_type n, const T& t = T{}) {create (n, T);} // fill
     Vector (const Vector& v) {create(v.begin(), v.end());} // copy konstruktorius
     template <class InputIterator>
-    Vector (InputIterator first, InputIterator last) {create(first, last);} //range konstruktorius
-    Vector (Vector && v) {create(); swap(v); v.uncreate();} //move konstruktorius
-    Vector (const std::initializer_list <T> il) {create(il.begin(), il.end());} //initializer list konstruktorius
-    ~Vector(){uncreate();} //destruktorius
-
+    Vector (InputIterator first, InputIterator last) {create(first, last);} // range konstruktorius
+    Vector (Vector && v) {create(); swap(v); v.uncreate();} // move konstruktorius
+    Vector (const std::initializer_list <T> il) {create(il.begin(), il.end());} // initializer list konstruktorius
+    ~Vector(){uncreate();} // destruktorius
+    //operator=
+    Vector& operator = (const Vector& other) {
+        if(this != &other){
+            uncreate();
+            create(other.begin(), other.end()); // copy assignment operatorius
+        }
+        return *this;
+    };
+    Vector& operator = (Vector&& other){
+        if (this != &other){
+            create(other.begin(), other.end()); // move assignment operatorius
+            uncreate();
+        }
+        return *this;
+    };
 };
 
 
