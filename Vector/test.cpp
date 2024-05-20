ce6db831ae41bca42e194583ff5b47d53dde4ec3
Pijus-B <p.bogusi@gmail.com>
2024-05-20 19:17:05 +0300

    tests

--- Vector/test.cpp
+++ Vector/test.cpp
@@ -1,64 +1,120 @@
 #include "pch.h"
-#include "C:\Users\Pijus\Desktop\folder\V1.5\2-uzduotis\Vector\studentas.h"
-TEST(StudentTest, ConstructorTest)
-{
-    vector <int> nd = { 95 };
-    Studentas student("Tomas", "Tedis", nd, 88, 90.5, 92.0, 91.5);
-    EXPECT_EQ(student.getVardas(), "Tomas");
-    EXPECT_EQ(student.getPavarde(), "Tedis");
-    EXPECT_EQ(student.getNd(), nd);
-    EXPECT_EQ(student.getEgz(), 88);
-    EXPECT_EQ(student.getBalas(), 90.5);
-    EXPECT_EQ(student.getVid(), 92.0);
-    EXPECT_EQ(student.getMediana(), 91.5);
-    cout << "Constructor sekmingai praejo" << endl;
-}
-TEST(StudentTest, CopyConstructor)
-{
-    vector <int> nd = { 90 };
-    Studentas originalas("Egle", "Lape", nd, 92, 91.5, 91.0, 90.5);
-    Studentas copy = originalas;
-    EXPECT_EQ(copy.getVardas(),"Egle");
-    EXPECT_EQ(copy.getPavarde(), "Lape");
-    EXPECT_EQ(copy.getNd(), nd);
-    EXPECT_EQ(copy.getEgz(), 92);
-    EXPECT_EQ(copy.getBalas(), 91.5);
-    EXPECT_EQ(copy.getVid(), 91.0);
-    EXPECT_EQ(copy.getMediana(), 90.5);
-    cout << "Copy constructor testas sekmingai praejo" << endl;
-}
-TEST(StudentTest, MoveConstructor)
-{
-    vector <int> nd = { 89 };
-    Studentas originalas("Akvile", "Aleksa", nd, 87, 88.0, 88.5, 87.5);
-    Studentas moved = move(originalas);
-    EXPECT_EQ(moved.getVardas(),"Akvile");
-    EXPECT_EQ(moved.getPavarde(), "Aleksa");
-    cout << "Move constructor sekmingai praejo" << endl;
-}
-TEST(StudentTest, CopyAssignment)
-{
-    vector <int> nd = { 85 };
-    Studentas originalas("Bobas", "Jonaitis", nd, 84, 84.5, 85.5, 85.0);
-    Studentas copy;
-    copy = originalas;
-    EXPECT_EQ(copy.getVardas(), "Bobas");
-    EXPECT_EQ(copy.getPavarde(), "Jonaitis");
-    EXPECT_EQ(copy.getNd(), nd);
-    EXPECT_EQ(copy.getEgz(), 84);
-    EXPECT_EQ(copy.getBalas(), 84.5);
-    EXPECT_EQ(copy.getVid(), 85.5);
-    EXPECT_EQ(copy.getMediana(), 85.0);
-    cout << "Copy assignment sekmingai praejo" << endl;
-}
-TEST(StudentTest, MoveAssignment)
-{
-    vector <int> nd = { 82 };
-    Studentas originalas("Marius", "Lelesius", nd, 80, 80.0, 82.5, 81.5);
-    Studentas moved;
-    moved = move(originalas);
-    EXPECT_EQ(moved.getVardas(), "Marius");
-    EXPECT_EQ(moved.getPavarde(), "Lelesius");
-    cout << "Move assignment sekmingai praejo" << endl;
+#include "vector.h"
 
+TEST(VectorTest, ConstructorTest)
+{
+    Vector<int> nd = { 95 };
+    EXPECT_EQ(nd.size(), 1);
+    EXPECT_EQ(nd.capacity(), 1);
+    EXPECT_EQ(nd[0], 95);
+}
+
+TEST(VectorTest, CopyConstructor)
+{
+    Vector<int> nd = { 90 };
+    Vector<int> copy = nd;
+    EXPECT_EQ(copy.size(), nd.size());
+    EXPECT_EQ(copy.capacity(), nd.capacity());
+    EXPECT_EQ(copy[0], nd[0]);
+}
+
+TEST(VectorTest, MoveConstructor)
+{
+    Vector<int> nd = { 89 };
+    Vector<int> moved = std::move(nd);
+    EXPECT_EQ(moved.size(), 1);
+    EXPECT_EQ(moved.capacity(), 1);
+    EXPECT_EQ(moved[0], 89);
+}
+
+TEST(VectorTest, CopyAssignment)
+{
+    Vector<int> nd = { 85 };
+    Vector<int> copy;
+    copy = nd;
+    EXPECT_EQ(copy.size(), nd.size());
+    EXPECT_EQ(copy.capacity(), nd.capacity());
+    EXPECT_EQ(copy[0], nd[0]);
+}
+
+
+TEST(VectorTest, AssignFunkcija) {
+    Vector<int> v;
+    v.assign(5, 10);
+    EXPECT_EQ(v.size(), 5);
+    EXPECT_EQ(v.capacity(), 5);
+    for (int i = 0; i < 5; ++i) {
+        EXPECT_EQ(v[i], 10);
+    }
+}
+
+TEST(VectorTest, AtFunkcija) {
+    Vector<int> v(5, 10);
+    for (int i = 0; i < 5; ++i) {
+        EXPECT_EQ(v.at(i), 10);
+    }
+    EXPECT_THROW(v.at(10), std::out_of_range);
+}
+
+TEST(VectorTest, FrontFunkcija) {
+    Vector<int> v = { 1, 2, 3, 4, 5 };
+    EXPECT_EQ(v.front(), 1);
+}
+
+TEST(VectorTest, BackFunkcija) {
+    Vector<int> v = { 1, 2, 3, 4, 5 };
+    EXPECT_EQ(v.back(), 5);
+}
+
+TEST(VectorTest, Size) {
+    Vector<int> v;
+    EXPECT_EQ(v.size(), 0);
+    v.push_back(10);
+    EXPECT_EQ(v.size(), 1);
+}
+
+TEST(VectorTest, Resize) {
+    Vector<int> v;
+    v.resize(5);
+    EXPECT_EQ(v.size(), 5);
+    v.resize(3);
+    EXPECT_EQ(v.size(), 3);
+}
+
+TEST(VectorTest, Capacity) {
+    Vector<int> v;
+    v.resize(10);
+    EXPECT_GE(v.capacity(), v.size());
+}
+
+TEST(VectorTest, Empty) {
+    Vector<int> v;
+    EXPECT_TRUE(v.empty());
+    v.push_back(5);
+    EXPECT_FALSE(v.empty());
+}
+
+TEST(VectorTest, Reserve) {
+    Vector<int> v;
+    v.reserve(100);
+    EXPECT_GE(v.capacity(), 100);
+}
+
+TEST(VectorTest, Clear) {
+    Vector<int> v;
+    v.push_back(5);
+    v.clear();
+    EXPECT_TRUE(v.empty());
+}
+
+TEST(VectorTest, LygusVektoriai) {
+    Vector<int> v1{ 1, 2, 3 };
+    Vector<int> v2{ 1, 2, 3 };
+    EXPECT_TRUE(v1 == v2);
+}
+
+TEST(VectorTest, NelygusVektoriai) {
+    Vector<int> v1{ 1, 2, 3 };
+    Vector<int> v2{ 1, 2, 4 };
+    EXPECT_TRUE(v1 != v2);
 }
